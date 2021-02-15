1cb8d118c44821880cb1214ce85334efef45955c
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-15 15:00:43 +0100

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
     default from: 'no-reply@monsite.fr'
 
+    layout "mailer"
     def welcome_email(user)
       #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
       @user = user
