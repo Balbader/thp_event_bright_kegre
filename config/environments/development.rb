7251bead35142e42e5fc5162b86770347dcbf094
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-15 15:27:02 +0100

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,8 +53,8 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Set the delivery method for Letter Opener
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true #set to false = turn off perform_deliverie
+  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.perform_deliveries = true #set to false = turn off perform_deliverie
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
