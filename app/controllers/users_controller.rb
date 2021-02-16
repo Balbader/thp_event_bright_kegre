356750ec374514b07eef9bc5025544b1e39f8dda
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-16 18:44:12 +0100

    update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+   #EMPECHER LE USER D'ALLER SUR UNE AUTRE PAGE PROFIL QUE LA SIENNE
+   before_action :authenticate_user!, :good_user
+
+   def index
+   end
+ 
+   def show
+     @user = User.find(current_user.id)
+     @event_admin = Event.all
+   end
+ 
+ private
+ 
+  def good_user
+   @user = User.find(params['id'])
+   if current_user.id == @user.id
+   else
+     redirect_to root_path
+     flash[:danger]= "Bien essayé petit malin ! ;P"
+   end
+  end
+end
